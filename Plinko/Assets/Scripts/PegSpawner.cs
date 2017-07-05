65579e8e37c4a54161e07b391d906ec2c75cfcb9
Julian Nicholls <jnicholls95@outlook.com>
2017-07-05 14:18:25 -0400

    Rage Quit
    Lost unsaved scene due to infinite loop (?)

---
+++ Plinko/Assets/Scripts/PegSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegSpawner : MonoBehaviour {
+
+	public GameObject[] pegVariety; 
+
+	public float startX;
+	public float startZ;
+
+	public float endX;
+	public float endZ;
+
+	public int densityX;
+	public int densityZ;
+
+	public bool pegsSpawned;
+
+
+	// Use this for initialization
+	void Start () {
+
+		for(int x = 0; x < densityX; x++){
+			for(int z = 0; x < densityZ; z++){
+
+				Vector3 position = new Vector3 (
+	            	Mathf.Lerp (startX, endX, x / densityX), 
+					0,
+					Mathf.Lerp (startZ, endZ, z / densityZ)
+				);
+
+				Instantiate (
+					pegVariety [Random.Range (0, pegVariety.Length)],
+					position,
+					new Quaternion(0, 0, 0, 0),
+					transform
+				);
+
+			}
+
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
